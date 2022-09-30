4814c45cc99e4960e1bba8901ffe8702d573d28d
djRemskii <jeremyong@cpp.edu>
2022-09-29 21:53:17 -0700

    Added all number buttons, going to work on the main boxes soon

--- HangmanDM/src/hangmandm/SudokuPanel.java
+++ HangmanDM/src/hangmandm/SudokuPanel.java
@@ -55,10 +55,81 @@ public class SudokuPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        num1 = new javax.swing.JButton();
+        num2 = new javax.swing.JButton();
+        num3 = new javax.swing.JButton();
+        num4 = new javax.swing.JButton();
+        num5 = new javax.swing.JButton();
+        num6 = new javax.swing.JButton();
+        num7 = new javax.swing.JButton();
+        num8 = new javax.swing.JButton();
+        num9 = new javax.swing.JButton();
         box0_0 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(600, 400));
+
+        num1.setText("1");
+        num1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num1ActionPerformed(evt);
+            }
+        });
+
+        num2.setText("2");
+        num2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num2ActionPerformed(evt);
+            }
+        });
+
+        num3.setText("3");
+        num3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num3ActionPerformed(evt);
+            }
+        });
+
+        num4.setText("4");
+        num4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num4ActionPerformed(evt);
+            }
+        });
+
+        num5.setText("5");
+        num5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num5ActionPerformed(evt);
+            }
+        });
+
+        num6.setText("6");
+        num6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num6ActionPerformed(evt);
+            }
+        });
+
+        num7.setText("7");
+        num7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num7ActionPerformed(evt);
+            }
+        });
+
+        num8.setText("8");
+        num8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num8ActionPerformed(evt);
+            }
+        });
+
+        num9.setText("9");
+        num9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                num9ActionPerformed(evt);
+            }
+        });
 
         box0_0.setEditable(false);
         box0_0.setBackground(new java.awt.Color(255, 255, 255));
@@ -75,40 +146,62 @@ public class SudokuPanel extends javax.swing.JPanel {
                 box0_0FocusLost(evt);
             }
         });
-        box0_0.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                box0_0MousePressed(evt);
-            }
-        });
-
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(244, 244, 244)
-                .addComponent(box0_0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(331, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(111, 111, 111))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(num1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(num2))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(num4)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(num5)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(num6)
+                                    .addComponent(num3)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(num7)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(num8)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(num9))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(181, 181, 181)
+                        .addComponent(box0_0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(394, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(161, 161, 161)
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(num1)
+                    .addComponent(num2)
+                    .addComponent(num3))
+                .addGap(7, 7, 7)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(num4)
+                    .addComponent(num5)
+                    .addComponent(num6))
+                .addGap(7, 7, 7)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(num7)
+                    .addComponent(num8)
+                    .addComponent(num9))
+                .addGap(17, 17, 17)
                 .addComponent(box0_0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 124, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(65, 65, 65))
+                .addContainerGap(240, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -125,23 +218,55 @@ public class SudokuPanel extends javax.swing.JPanel {
         }
     }
     
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        setBox(2);
-        //boxes[focusedBox[0]][focusedBox[1]].setText("2");
-        boxes[0][0].setText("2");
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
     Border redBorder = BorderFactory.createLineBorder(Color.RED,3);
     
-    private void box0_0MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_box0_0MousePressed
-        // TODO add your handling code here:
-        //box0_0.setBorder(redBorder);
-    }//GEN-LAST:event_box0_0MousePressed
+    private void num1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num1ActionPerformed
+        setBox(1);
+        boxes[focusedBox[0]][focusedBox[1]].setText("1");
+    }//GEN-LAST:event_num1ActionPerformed
+
+    private void num2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num2ActionPerformed
+        setBox(2);
+        boxes[focusedBox[0]][focusedBox[1]].setText("2");
+    }//GEN-LAST:event_num2ActionPerformed
+
+    private void num3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num3ActionPerformed
+        setBox(3);
+        boxes[focusedBox[0]][focusedBox[1]].setText("3");
+    }//GEN-LAST:event_num3ActionPerformed
+
+    private void num4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num4ActionPerformed
+        setBox(4);
+        boxes[focusedBox[0]][focusedBox[1]].setText("4");
+    }//GEN-LAST:event_num4ActionPerformed
+
+    private void num5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num5ActionPerformed
+        setBox(5);
+        boxes[focusedBox[0]][focusedBox[1]].setText("5");
+    }//GEN-LAST:event_num5ActionPerformed
+
+    private void num6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num6ActionPerformed
+        setBox(6);
+        boxes[focusedBox[0]][focusedBox[1]].setText("6");
+    }//GEN-LAST:event_num6ActionPerformed
+
+    private void num7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num7ActionPerformed
+        setBox(7);
+        boxes[focusedBox[0]][focusedBox[1]].setText("7");
+    }//GEN-LAST:event_num7ActionPerformed
+
+    private void num8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num8ActionPerformed
+        setBox(8);
+        boxes[focusedBox[0]][focusedBox[1]].setText("8");
+    }//GEN-LAST:event_num8ActionPerformed
+
+    private void num9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_num9ActionPerformed
+        setBox(9);
+        boxes[focusedBox[0]][focusedBox[1]].setText("9");
+    }//GEN-LAST:event_num9ActionPerformed
 
     private void box0_0FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_box0_0FocusGained
-        // TODO add your handling code here:
         box0_0.setBorder(redBorder);
         focusedBox[0] = 0;
         focusedBox[1] = 0;
@@ -149,13 +274,20 @@ public class SudokuPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_box0_0FocusGained
 
     private void box0_0FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_box0_0FocusLost
-        // TODO add your handling code here:
         box0_0.setBorder(blackBorder);
     }//GEN-LAST:event_box0_0FocusLost
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private static javax.swing.JTextField box0_0;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton num1;
+    private javax.swing.JButton num2;
+    private javax.swing.JButton num3;
+    private javax.swing.JButton num4;
+    private javax.swing.JButton num5;
+    private javax.swing.JButton num6;
+    private javax.swing.JButton num7;
+    private javax.swing.JButton num8;
+    private javax.swing.JButton num9;
     // End of variables declaration//GEN-END:variables
 }
