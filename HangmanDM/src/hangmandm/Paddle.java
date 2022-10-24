fb3533603819f2d813fc9fe04306bc711c3bf676
Kevin Hoang <hevinkoang@gmail.com>
2022-10-23 21:49:54 -0700

    Added border restrictions

--- HangmanDM/src/hangmandm/Paddle.java
+++ HangmanDM/src/hangmandm/Paddle.java
@@ -13,6 +13,7 @@ public class Paddle {
     int player, x;
     public Paddle(int player){
         y = 200;
+        y2 = 200;
         if(player == 1)
             x = 20;
         else
@@ -26,17 +27,32 @@ public class Paddle {
     }
     public static void moveUp(){
         y -= 20;
-
+        if(y > 280)
+            y = 280;
+        if(y < 5)
+            y = 5;
     }
     public static void moveDown(){
         y += 20;
+        if(y > 280)
+            y = 280;
+        if(y < 5)
+            y = 5;
     }
         public static void moveUp2(){
         y2 -= 20;
+        if(y2 > 280)
+            y2 = 280;
+        if(y2 < 5)
+            y2 = 5;
 
     }
     public static void moveDown2(){
         y2 += 20;
+        if(y2 > 280)
+            y2 = 280;
+        if(y2 < 5)
+            y2 = 5;
     }
     public int getY(){
         return (int)y;
